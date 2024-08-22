dd2838bc7c9a808fb513a9fc8785e084da675047
Pavel Martynenkov <pashok.resh@gmail.com>
2024-08-22 09:14:43 +0200

    initial project created

---
+++ ToDoPlaner/ToDoPlaner/ToDoPlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoPlanerApp.swift
+//  ToDoPlaner
+//
+//  Created by Pavel Martynenkov on 22.08.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoPlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
